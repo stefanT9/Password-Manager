ac58794966a05dd4c3caec127838593e0f040da8
Stefan <stefan.tomsa99@gmail.com>
2020-12-06 14:55:31 +0200

    initial commit with project structure

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+
+args = sys.argv
+
+master_password = args[1]
+operation =  args[2]
+
+
+def list_command():
+    continue
+def get_command():
+    continue
+def add_command():
+    continue
+def remove_command():
+    continue
+
+if action=='-list':
+    continue
+elif action=='-add':
+    website  = args[3]
+    username = args[4]
+    password = args[5]
+
+elif action=='-get':
+    website  = args[3]
+
+elif action=='-remove':
+    website  = args[3]
