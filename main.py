1df9409b741d2085c22b34dc7ad386137338942e
Stefan <stefan.tomsa99@gmail.com>
2020-12-06 15:30:33 +0200

    added the correct variable names to the basic structure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from cryptography.fernet import Fernet
 
 args = sys.argv
 
@@ -6,24 +7,49 @@ master_password = args[1]
 operation =  args[2]
 
 
-def list_command():
-    continue
-def get_command():
-    continue
-def add_command():
-    continue
-def remove_command():
-    continue
+def authenticate(password):
+    return True
 
-if action=='-list':
-    continue
-elif action=='-add':
+def encrypt(text, key):
+    f = Fernet(key)
+    return f.encrypt(text)
+    
+def decrypt(text, key):
+    f = Fernet(key)
+    return f.decrypt(text)
+
+def init_command(master_password):
+    f = open('passwords.txt','a+')
+    print(f)
+    f.close()
+    return
+
+def list_command(master_password):
+    return []
+
+def get_command(master_password, website):
+    return ''
+
+def add_command(master_password, website, email, password):
+    return
+
+def remove_command(master_password, website):
+    return
+if   operation=='-init':
+    init_command(master_password)
+
+elif operation=='-list':
+    list_command(master_password)
+elif operation=='-add':
     website  = args[3]
     username = args[4]
     password = args[5]
+    add_command(master_password,website,username,password)
 
-elif action=='-get':
+elif operation=='-get':
     website  = args[3]
+    get_command(master_password, website)
 
-elif action=='-remove':
+elif operation=='-remove':
     website  = args[3]
+    remove_command(master_password, website)
